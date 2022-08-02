552b53bd6b07d0f6fc77f46fbdc7838cb71dcbc2
Eyas Sharary <eyas.wt@gmail.com>
2022-08-02 20:47:23 +0300

    tring to fix chats button active on render issue

--- client/src/Nav/Nav.jsx
+++ client/src/Nav/Nav.jsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import { AudioOutlined ,SearchOutlined } from '@ant-design/icons';
+import React, { useRef } from 'react';
 import './Nav.css'
-import { Link, Route, Router, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Nav(props) {
    
+    const  chatsRef=useRef();
+    
     return (
         <div className='Nav-Container'>
-            <input placeholder="Search..." type={"text"} className="Search" 
-                ></input>
+            <input placeholder="Search...
+            " type={"text"} 
+            className="Search"
+            
+            ></input>
              
             
             <div className='Op-Container'>
             
             <Link to='/User/Home/Chats'>
                 <button 
-                
+                ref={chatsRef}
                 className='Chat-Btn' 
                 >Chats</button>
             </Link>
+            
 
             <Link to='/User/Home/Contacts'> 
                 <button className='Contact-Btn'> Contacts</button>
@@ -26,7 +31,7 @@ function Nav(props) {
             
             </div>
             
-
+            
 
 
         </div>
