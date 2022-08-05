715c50c96e6213302e0109ffe426158c6b6452ef
Eyas Sharary <eyas.wt@gmail.com>
2022-08-05 13:24:50 +0300

    more UI progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Login from './LogIn/login';
 import SignUp from './SignUp/SignUp';
-import User from './User/User';
 import Home from './Home/Home'
 import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 
@@ -12,16 +11,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Login />}></Route>
           <Route exact path="/Signup" element={<SignUp />}></Route>
-          <Route exact path='/User' element={<User />}></Route>
-          <Route exact path='/User/Home' element={<Home />}>
-            <Route exact path='/User/Home/Chats' 
-              element={<h1>chats work</h1>}>
-            </Route>
-            <Route exact path='/User/Home/Contacts'
-              element={<h1>contacts work</h1>}>
-            </Route>
+          <Route exact path='/App' element={<Home/>}>
           </Route>
-          
         </Routes>
       </Router>
     </div>
--- client/src/Nav/Nav.jsx
+++ client/src/Nav/Nav.jsx
@@ -1,33 +1,95 @@
-import React, { useRef } from 'react';
-import './Nav.css'
-import { Link, useLocation } from 'react-router-dom';
-import {MessageOutlined, ContactsOutlined, UserOutlined} from '@ant-design/icons';
-function Nav(props) {
-   
-    const  chatsRef=useRef();
-    const location = useLocation()
+import React, { Component, useState } from 'react';
+import "./Nav.css"
+import {SearchOutlined, ArrowLeftOutlined} from '@ant-design/icons';
+import { format } from 'timeago.js';
+
+export default function Nav(props) {
+    const [searchFeild, setSearchFeild] = useState("")
+
+    const handlechange = e => {
+        setSearchFeild(e.target.value)
+    }
+
+    
+
+
+    const chatsJSX = []
+    const chats = props.chats
+    if (searchFeild.trim() === "") {
+        for (let index = 0; index < chats.length; index++) {
+            const chat = chats[index];
+            chatsJSX.push(
+                <div className="chat">
+                    <img src={chat.picture} alt="" className="profilePic" />
+                    <span className='contactName'>{chat.name}</span>
+                    <span className='date'>{format(chat.messages[chat.messages.length -1].createdAt)}</span>
+                    <span className='lastMessage'>{chat.messages[chat.messages.length -1].content}</span>
+                </div>
+            )
+        }
+    } else {
+        for (let index = 0; index < chats.length; index++) {
+            const chat = chats[index];
+            if (!chat.name.toLowerCase().includes(searchFeild.toLowerCase())) continue;
+            chatsJSX.push(
+                <div className="chat">
+                    <img src={chat.picture} alt="" className="profilePic" />
+                    <span className='contactName'>{chat.name}</span>
+                    <span className='date'>{format(chat.messages[chat.messages.length -1].createdAt)}</span>
+                    <span className='lastMessage'>{chat.messages[chat.messages.length -1].content}</span>
+                </div>
+            )
+        }
+    }
+
+    const openProfile = e => toggleProfile(true)
+    const closeProfile = e => toggleProfile(false)
+
+    const toggleProfile = open =>{
+        const picture = document.querySelector(".profileBtn")
+        const header = document.querySelector(".header")
+        const chatsWrapper = document.querySelector(".chatsWrapper")
+        const profileInfo = document.querySelector(".profileInfo")
+        const profileBackButton = document.querySelector(".profileBackButton")
+        if (open){
+            header.classList.add("hidden")
+            chatsWrapper.classList.add("hidden")
+            picture.classList.add("profileBtnOpen")
+            profileInfo.classList.remove("hidden")
+            profileBackButton.classList.remove("hiddenLeft")
+        } else {
+            header.classList.remove("hidden")
+            chatsWrapper.classList.remove("hidden")
+            picture.classList.remove("profileBtnOpen")
+            profileInfo.classList.add("hidden")
+            profileBackButton.classList.add("hiddenLeft")
+        }
+    }
 
     return (
-        <div className='Nav-Container'>
-            <Link to="/user/home/chats">
-                <span className={"navButton " + (location.pathname == "/user/home/chats" ? "active" : "")} id='btnChats'>
-                    <MessageOutlined />
+      <div className='Nav'>
+        <img src="https://instadownloader.co/view_photo.php?url=https%3A//instagram.fsdv1-2.fna.fbcdn.net/v/t51.2885-15/292586271_583938633454985_2981626577624270745_n.webp%3Fstp%3Ddst-jpg_e35%26_nc_ht%3Dinstagram.fsdv1-2.fna.fbcdn.net%26_nc_cat%3D105%26_nc_ohc%3D2Heioy5npy4AX_Ik2Th%26edm%3DAABBvjUBAAAA%26ccb%3D7-5%26ig_cache_key%3DMjg3ODU0NDc3MzU5NDkxOTE0NQ%253D%253D.2-ccb7-5%26oh%3D00_AT9wYxwOyXsIQJYA0JOJ3tFQtMJl0VcLaYQ_0Q-vo3gTVw%26oe%3D62F2CEDB%26_nc_sid%3D83d603" alt="" className="profileBtn" onClick={openProfile} />
+        <div className="header">
+            <h1 className="chatsTitle">Chats</h1>
+            <div className="searchBar">
+                <span className='searchIcon'>
+                    <SearchOutlined />
                 </span>
-            </Link>
-
-            <Link to="/user/home/contacts">
-                <span className={"navButton " + (location.pathname == "/user/home/contacts" ? "active" : "")} id='btnContacts'>
-                    <ContactsOutlined />
-                </span>
-            </Link>
-
-            <Link to="/user">
-                <span className={"navButton " + (location.pathname == "/user" ? "active" : "")} id="btnProfile">
-                    <UserOutlined />
-                </span>
-            </Link>
+                <input placeholder='Search...' type="text" className="searchInput" onChange={handlechange} />
+            </div>
         </div>
+
+        <div className="chatsWrapper">
+            {chatsJSX}
+        </div>
+
+        <div className="profile">
+            <span className="profileBackButton hiddenLeft" onClick={closeProfile}><ArrowLeftOutlined /></span>
+            <div className="profileInfo hidden">
+                <span className="profileName">Name: Eyas Sharary</span>
+                <span className="profileId">ID: eyas_sharary</span>
+            </div>
+        </div>
+      </div>
     );
 }
-
-export default Nav; 
